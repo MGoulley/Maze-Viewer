020dd4e3794cbe5ea5d508a4294b0067cce1b1d8
mattg <mattg@192.168.1.10>
2019-01-11 17:41:13 +0100

    First commit

---
+++ Maze_Viewer/src/Main.java
@@ -0,0 +1,20 @@
+import javax.swing.JFrame;
+
+public class Main {
+	public static void main(String[] args) {
+		// 1. Create the frame.
+		JFrame frame = new JFrame("FrameDemo");
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		// 3. Create components and put them in the frame.
+		frame.getContentPane();
+
+		// 4. Size the frame.
+		frame.pack();
+
+		// 5. Show it.
+		frame.setVisible(true);
+	}
+
+}
